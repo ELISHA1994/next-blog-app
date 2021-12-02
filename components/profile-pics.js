eb0b0931c0cadf5d6dbaed874dc5545f6239846b
Elisha Bello <elishabello2014@gmail.com>
2021-12-02 09:12:43 +0100

    Navigate Between Pages &&  Assets, Metadata, and CSS

---
+++ components/profile-pics.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+const ProfilePics = () => (
+    <Image
+        src="/images/profile.jpg"
+        height={3072}
+        width={4096}
+        alt="Your Name"
+    />
+);
